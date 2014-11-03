942f7f302dd32c78a7e92d78a9e217fed365a2cc
matthewfjerstad <matthew.fjerstad@jamfsoftware.com>
2014-11-03 10:32:43 -0600

    major changes to structure. added client check ins

--- jssLoadSimulator.py
+++ jssLoadSimulator.py
@@ -22,90 +22,103 @@ enrolledComputers = []
 number_of_new_computers = ''
 number_of_times_to_update = ''
 time_between_updates = ''
+checkin_or_update = ''
 jss_host = ""
 jss_port = ""
 jss_path = ""
 jss_username = ""
 jss_password = ""
-initial_computer_id = ""
 
 
 def main(argv):
     verify_variables(argv)
     verify_jss_details()
     computer_record = get_initial_computer()
-    computer_record = computer_record.replace("<id>" + str(initial_computer_id) + "</id>", "")
+    checkin_string = get_checkin_string()
     computers = []
     x = 0
     while x < int(number_of_new_computers):
-        c = Computer(computer_record, x)
+        c = Computer(computer_record, checkin_string, x)
         computers.append(SubmitThread(c))
         x += 1
     for comp in computers:
         comp.start()
     x = 0
     while x < int(number_of_times_to_update):
+        print "Waiting " + str(time_between_updates) + " seconds before next update."
+        time.sleep(int(time_between_updates))
         for comp in enrolledComputers:
             c = SubmitThread(comp)
             c.start()
-        print "Waiting " + str(time_between_updates) + " seconds before next update."
-        time.sleep(int(time_between_updates))
         x += 1
     write_settings_to_plist()
 
 
 class Computer():
-    def __init__(self, detail_string, index):
-        self.detail_string = randomize_computer(detail_string)
+    def __init__(self, detail_string, check_in_string, index):
+        self.serial_number = random.randint(9999999999, 99999999999)
+        clean_string = re.sub(r'<serial_number></serial_number>',
+                          '<serial_number>' + str(self.serial_number) + '</serial_number>',
+                          detail_string, )
+        self.udid = uuid.uuid1()
+        clean_string = re.sub(r'<udid></udid>', '<udid>' + str(self.udid) + '</udid>',
+                          clean_string, )
+        self.mac_address = random_mac()
+        clean_string = re.sub(r'<mac_address></mac_address>',
+                          '<mac_address>' + str(self.mac_address) + '</mac_address>', clean_string, )
+        self.alt_mac_address = random_mac()
+        clean_string = re.sub(r'<alt_mac_address></alt_mac_address>',
+                        '<alt_mac_address>' + str(self.alt_mac_address) + '</alt_mac_address>', clean_string, )
+        name_file = open('/Users/matthewfjerstad/Documents/LoadSimulator/names')
+        name_list = name_file.readlines()
+        name_file.close()
+        username = name_list[random.randint(0, len(name_list) - 1)].strip()
+        computer_name = username \
+                        + "'s MacBook Air"
+        clean_string = re.sub(r'COMPUTERNAME', '<name>' + str(computer_name) + '</name>', clean_string, )
+        location = '<username>' + username + str(random.randint(999, 9999)) + '</username><realname/><email_address>' + username.lower() + \
+            '@jamf.com</email_address>'
+        clean_string = re.sub(r'LOCATIONINFO', location, clean_string, )
+        self.detail_string = clean_string
+        clean_checkin = re.sub(r'UDID', '<uuid>' + str(self.udid) + '</uuid>', check_in_string, )
+        clean_checkin = re.sub(r'MACADDRESS', '<macAddress bsdName="en0">' + str(self.mac_address) + '</macAddress>', clean_checkin, )
+        self.checkin_string = clean_checkin
         self.index = index
         self.computer_id = 0
+        global enrolledComputers
+        enrolledComputers.append(self)
 
     def submit(self):
+        global checkin_or_update
         if self.computer_id == 0:
             print "Starting to create computer: " + str(self.index)
+            device_response = connect_jss("/JSSResource/computers", "POST", self.detail_string)
+            device_response = re.sub('"','\"',device_response,)
             try:
-                url = "https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + "/JSSResource/computers"
-                opener = urllib2.build_opener(urllib2.HTTPHandler)
-                request = urllib2.Request(url, self.detail_string)
-                request.add_header("Authorization", get_auth_header(jss_username, jss_password))
-                request.add_header('Content-Type', 'application/xml')
-                request.get_method = lambda: 'POST'
-                response = opener.open(request)
-                try:
-                    device = xml.dom.minidom.parseString(response.read())
-                    self.computer_id = device.getElementsByTagName('id')[0].childNodes[0].data
-                except:
-                    print "Failure parsing computer POST response"
-                enrolledComputers.append(self)
-                print "Finished creating computer: " + str(self.computer_id)
-            except httplib.HTTPException as inst:
-                print "\tException: %s" % inst
-            except ValueError as inst:
-                print "\tException submitting POST XML: %s" % inst
-            except urllib2.HTTPError as inst:
-                print "\tException submitting POST XML: %s" % inst
+                device = xml.dom.minidom.parseString(str(device_response))
+                self.computer_id = device.getElementsByTagName('id')[0].childNodes[0].data
             except:
-                print "\tUnknown error submitting POST XML"
-        else:
+                print "Computer Creation failed"
+        elif checkin_or_update == 'u':
             print "Starting to update computer: " + str(self.computer_id)
-            try:
-                url = "https://" + str(jss_host) + ":" + str(jss_port) + str(
-                    jss_path) + "/JSSResource/computers/id/" + str(self.computer_id)
-                opener = urllib2.build_opener(urllib2.HTTPHandler)
-                request = urllib2.Request(url, self.detail_string)
-                request.add_header("Authorization", get_auth_header(jss_username, jss_password))
-                request.add_header('Content-Type', 'application/xml')
-                request.get_method = lambda: 'PUT'
-                opener.open(request)
+            update_response = connect_jss("/JSSResource/computers/id/" + str(self.computer_id), "PUT", self.detail_string)
+            if xml.dom.minidom.parseString(update_response) == self.computer_id:
                 print "Finished updating computer: " + str(self.computer_id)
-            except httplib.HTTPException as inst:
-                print "\tException: %s" % inst
-            except ValueError as inst:
-                print "\tException submitting PUT XML: %s" % inst
-            except urllib2.HTTPError as inst:
-                print "\tException submitting PUT XML: %s" % inst
+        elif checkin_or_update == 'c':
+            print "Starting to check in computer: " + str(self.computer_id)
+            checkin_response = connect_jss_client("/client", "POST", self.checkin_string)
+            try:
+                response_parse = xml.dom.minidom.parseString(checkin_response.read())
+                response_id = response_parse.getElementsByTagName('code')[0].childNodes[0].data
+                if str(response_id) == "1501":
+                    print "Check in failed for computer: " + self.computer_id + \
+                        "\tMake sure Cert Based Communication is disabled"
+                elif str(response_id) == "0":
+                    print "Check in successful for computer: " + self.computer_id
             except:
-                print "\tUnknown error submitting PUT XML"
+                print "failed"
+        else:
+            print 'Didn\'t create or update'
 
 
 class SubmitThread(threading.Thread):
@@ -119,12 +132,13 @@ class SubmitThread(threading.Thread):
 
 def verify_variables(argv):
     try:
-        opts, args = getopt.getopt(argv, "n:u:d:", ["n=", "u=", "d="])
+        opts, args = getopt.getopt(argv, "n:u:d:o:", ["n=", "u=", "d=", "o="])
     except getopt.GetoptError:
         print "Incorrect syntax\n" \
               "jssLoadSimulator -n <number of new computers>\n" \
               "\t-u <number of times to update>\n" \
-              "\t-d <time in seconds between delay>\n"
+              "\t-d <time in seconds between delay>\n" \
+              "\t-o <option for update 'u' or check in 'c'>"
         sys.exit(2)
     for opt, arg in opts:
         if opt == "-n":
@@ -136,11 +150,16 @@ def verify_variables(argv):
         elif opt == "-d":
             global time_between_updates
             time_between_updates = arg
-    if number_of_times_to_update == '' or number_of_times_to_update == '' or time_between_updates == '':
+        elif opt == "-o":
+            global checkin_or_update
+            checkin_or_update = arg
+    if number_of_times_to_update == '' or number_of_times_to_update == '' or time_between_updates == '' \
+            or checkin_or_update == '':
         print "Incorrect syntax\n" \
               "jssLoadSimulator -n <number of new computers>\n" \
               "\t-u <number of times to update>\n" \
-              "\t-d <time in seconds between delay>\n"
+              "\t-d <time in seconds between delay>\n" \
+              "\t-o <option for update 'u' or check in 'c'>"
         sys.exit(2)
 
 
@@ -149,22 +168,18 @@ def verify_jss_details():
     global jss_host
     global jss_port
     global jss_path
-    global initial_computer_id
     file_path = os.path.expanduser("~/Library/Preferences/com.jamfsoftware.jssloadsimulator.plist")
     if os.path.isfile(file_path):
         jss_info = plistlib.readPlist(file_path)
         jss_host = jss_info['jss_host']
         jss_port = jss_info['jss_port']
         jss_path = jss_info['jss_path']
-        initial_computer_id = jss_info['initial_computer_id']
     if jss_host == '':
             jss_host = prompt_user('JSS Hostname')
     if jss_port == '':
             jss_port = prompt_user('JSS Port')
     if jss_path == '' and not os.path.isfile(file_path):
             jss_path = prompt_user('JSS path (ex https://jss.com:8443/apple enter apple\n JSS path')
-    if initial_computer_id == '':
-            initial_computer_id = prompt_user("initial computer's JSS ID")
     global jss_username
     jss_username = prompt_user('JSS Username')
     global jss_password
@@ -181,45 +196,31 @@ def get_auth_header(u, p):
 
 
 def get_initial_computer():
-    headers = {"Authorization": get_auth_header(jss_username, jss_password), "Accept": "application/xml"}
-    try:
-        conn = httplib.HTTPSConnection(jss_host, jss_port)
-        conn.request("GET", jss_path + "/JSSResource/computers/id/" + str(initial_computer_id), None, headers)
-        data = conn.getresponse().read()
-        conn.close()
-        return data
-    except httplib.HTTPException as inst:
-        print "Could not get first computer's details. Exception: %s" % inst
+    computer_file = open("/Users/matthewfjerstad/Documents/LoadSimulator/computerXML")
+    computer_string = computer_file.read()
+    computer_file.close()
+    return computer_string
+
+def get_checkin_string():
+    checkin_file = open("/Users/matthewfjerstad/Documents/LoadSimulator/computerCheckin")
+    checkin_string = checkin_file.read()
+    checkin_file.close()
+    return checkin_string
 
 
 def write_settings_to_plist():
     global jss_host
     global jss_port
     global jss_path
-    global initial_computer_id
     file_path = os.path.expanduser("~/Library/Preferences/com.jamfsoftware.jssloadsimulator.plist")
     plist = { 'jss_host': jss_host,
               'jss_port': jss_port,
-              'jss_path': jss_path,
-              'initial_computer_id': initial_computer_id}
+              'jss_path': jss_path}
     try:
         print "Saving JSS settings"
         plistlib.writePlist(plist, file_path)
     except TypeError as inst:
         print "\tError writing plist: %s" % inst
-
-
-def randomize_computer(computer_string):
-    clean_string = re.sub(r'<serial_number>...........</serial_number>',
-                          '<serial_number>' + str(random.randint(9999999999, 99999999999)) + '</serial_number>',
-                          computer_string, )
-    clean_string = re.sub(r'<udid>....................................</udid>', '<udid>' + str(uuid.uuid1()) + '</udid>',
-                          clean_string, )
-    clean_string = re.sub(r'<mac_address>.................</mac_address>',
-                          '<mac_address>' + str(random_mac()) + '</mac_address>', clean_string, )
-    clean_string = re.sub(r'<alt_mac_address>.................</alt_mac_address>',
-                          '<alt_mac_address>' + str(random_mac()) + '</alt_mac_address>', clean_string, )
-    return clean_string
 
 
 def random_mac():
@@ -229,6 +230,44 @@ def random_mac():
            random.randint(0x00, 0xff)]
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
+
+def connect_jss(path, method, body):
+    try:
+        url = "https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + str(path)
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request(url, body)
+        request.add_header("Authorization", get_auth_header(jss_username, jss_password))
+        request.add_header('Content-Type', 'application/xml')
+        request.get_method = lambda: str(method)
+        response = opener.open(request)
+        return response.read()
+    except httplib.HTTPException as inst:
+        print "\tException: %s" % inst
+    except ValueError as inst:
+        print "\tException submitting " + str(method) + " XML: " + str(inst)
+    except urllib2.HTTPError as inst:
+        print "\tException submitting " + str(method) + " XML: " + str(inst)
+    except:
+        print "\tUnknown error submitting " + str(method) + " XML"
+
+
+def connect_jss_client(path, method, body):
+    try:
+        url = "https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + str(path)
+        opener = urllib2.build_opener(urllib2.HTTPHandler)
+        request = urllib2.Request(url, body)
+        request.add_header('Content-Type', 'application/xml')
+        request.get_method = lambda: str(method)
+        response = opener.open(request)
+        return response
+    except httplib.HTTPException as inst:
+        print "\tException: %s" % inst
+    except ValueError as inst:
+        print "\tException submitting " + str(method) + " XML: " + str(inst)
+    except urllib2.HTTPError as inst:
+        print "\tException submitting " + str(method) + " XML: " + str(inst)
+    except:
+        print "\tUnknown error submitting " + str(method) + " XML"
 
 if __name__ == "__main__":
     main(sys.argv[1:])
