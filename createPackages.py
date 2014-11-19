37cd7ae5234f23d4bc4ba9fb4ab8ab8157cae2f6
matthewfjerstad <matthew.fjerstad@jamfsoftware.com>
2014-11-19 13:32:20 -0600

    Added bulk package creation

---
+++ createPackages.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+import ssl
+import getpass
+import random
+
+
+jss_host = ""
+jss_port = ""
+jss_path = ""
+jss_username = ""
+jss_password = ""
+number_to_create = 0
+
+
+def main():
+    x = 0
+    verify_jss_details()
+    while x < number_to_create:
+        x += 1
+        create_package()
+    print 'Finished creating ' + str(x) + ' packages.\n'
+
+
+
+def verify_jss_details():
+
+    global jss_host
+    global jss_port
+    global jss_path
+    if jss_host == '':
+            jss_host = prompt_user('JSS Hostname')
+    if jss_port == '':
+            jss_port = prompt_user('JSS Port')
+    if jss_path == '':
+            jss_path = prompt_user('JSS path (ex https://jss.com:8443/apple enter apple\n JSS path')
+    global jss_username
+    jss_username = prompt_user('JSS Username')
+    global jss_password
+    jss_password = getpass.getpass('Enter JSS Password: ')
+    global number_to_create
+    number_to_create = int(prompt_user('Enter number of packages to create: '))
+
+
+def create_package():
+    package_name = str(random.randint(9999, 999999))
+    package_string = '<package><name>package' + package_name + '</name><filename>' + package_name + '.dmg</filename></package>'
+    connect_jss('/JSSResource/packages', 'POST', package_string)
+
+
+def prompt_user(variable):
+    return raw_input('Enter ' + variable + ': ')
+
+
+
+class MyAdapter(HTTPAdapter):
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = PoolManager(num_pools=connections, maxsize=maxsize, block=block,ssl_version=ssl.PROTOCOL_TLSv1)
+
+
+def connect_jss(path, method, body):
+    try:
+        session = requests.Session()
+        session.mount("https://" + str(jss_host) + ":" + str(jss_port), MyAdapter())
+        session.auth = (jss_username, jss_password)
+        session.headers.update({'Content-Type':'application/xml'})
+
+        if method == 'GET':
+            response = session.get("https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + str(path))
+        elif method == 'POST':
+            response = session.post("https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + str(path), data=body)
+        elif method == 'PUT':
+            response = session.put("https://" + str(jss_host) + ":" + str(jss_port) + str(jss_path) + str(path), data=body)
+        return response.text
+    except requests.exceptions.RequestException as e:
+        print "Connection exception: " + str(e)
+
+
+main()
